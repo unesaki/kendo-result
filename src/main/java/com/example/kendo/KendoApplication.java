8097b7d72167fe7a18386ef55ab51dae14606053
e un <eun@enoMacBook-Pro.local>
2025-03-28 21:56:34 +0900

    初回コミット

---
+++ src/main/java/com/example/kendo/KendoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KendoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kendo/KendoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
